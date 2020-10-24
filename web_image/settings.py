03afcae5ba86bc236fa993d5791ffe0b6276e884
Diego Ayala <dieguito9677@gmail.com>
2020-10-23 23:08:09 -0400

    add static files

--- web_image/settings.py
+++ web_image/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/home/ubuntu/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # media config
 MEDIA_URL = '/media/'
